2b18842e0a76543870767e076d0a1d4df8a43d30
Jessica Leach <jessica@info.nl>
2017-12-09 21:07:58 +0100

    Use postgres as production database

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'pages/info'
+
+  root to: redirect('/ideas')  
+  resources :ideas
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
