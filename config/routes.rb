ba5a3d27cf67746bb24671d80a9b1b5b98935f66
Ananya <ananyasingh7991@gmail.com>
2018-03-03 01:14:15 +0530

    Added lesson route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :lessons
   devise_for :users
   get 'pages/info'
-
+  get '/lessons/:id', to: 'lessons#show'
   root to: redirect('/ideas')  
   resources :ideas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
