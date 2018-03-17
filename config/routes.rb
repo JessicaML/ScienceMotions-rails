d891fe574ddb573b765a186aaa6baca7dbdfb391
Jessica Leach <jessicamaryleach@gmail.com>
2018-03-17 10:20:57 +0100

    added redirect route to lessons

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   get 'pages/info'
 
-  root to: redirect('/ideas')  
+  root to: redirect('/lessons')  
   resources :ideas
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
