0cb6aea0adb4b51b71308e485ece43f433e6bc2d
Jessica Leach <jessicamaryleach@gmail.com>
2018-04-08 17:08:46 +0200

    saves both ids

--- app/controllers/completed_lessons_controller.rb
+++ app/controllers/completed_lessons_controller.rb
@@ -25,7 +25,13 @@ class CompletedLessonsController < ApplicationController
   # POST /completed_lessons.json
   def create
     # @completed_lesson = CompletedLesson.new(completed_lesson_params)
-    @completed_lesson = current_user.completed_lessons.new(params[:completed_lesson_params])
+    # @completed_lesson = current_user.completed_lessons.new(params[:completed_lesson_params])
+
+    @completed_lesson = CompletedLesson.new(completed_lesson_params)
+    @completed_lesson.user = current_user
+    @completed_lesson.save
+    # .. save the completed lesson ..
+
 
     respond_to do |format|
 
