5cb2272f71b21f5be701fdb348d3177a5475484e
Jessica Leach <jessicamaryleach@gmail.com>
2018-04-12 18:53:44 -0400

    lesson.name if lesson

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Completed_lesson.delete_all
-Lesson.delete_all
-User.delete_all
+# Completed_lesson.delete_all
+# Lesson.delete_all
+# User.delete_all
 
 #Users
 vanessa = User.create(email: "van@gmail.com", password: "van")
 
 #Lessons
 lesson1 = Lesson.create(name: "van", description: "van", slug: "van")
+
+lesson2 = Completed_lesson.create(user_id: "1", lesson_id: "1", completed: "true")
