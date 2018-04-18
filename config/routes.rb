91892309fd0ef1d9a8a6cd4966f13725c5d66a53
paarmita <paarmita1998@gmail.com>
2018-04-18 15:19:50 +0530

    static index page

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,6 @@ Rails.application.routes.draw do
   root to: redirect('/lessons')  
   resources :lessons
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get "/static/:index" => "static#show"
 end
 
