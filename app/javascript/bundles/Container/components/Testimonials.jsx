a3165ab47b534e6c329f60988f12d45806501c84
Jessica Leach <jessicamaryleach@gmail.com>
2018-05-10 20:17:31 +0200

    renders all the components in container, testimonials renders

--- app/javascript/bundles/Container/components/Testimonials.jsx
+++ app/javascript/bundles/Container/components/Testimonials.jsx
@@ -23,11 +23,28 @@ export default class Testimonials extends React.Component {
 
   render() {
     return (
-        <div id="container" data-0="background-image:linear-gradient(90deg, hsl(172, 100%, 50%), hsl(318, 68%, 68%);" data-450="background-image:linear-gradient(450deg, hsl(172, 100%, 50%), hsl(318, 68%, 68%);">
-
-        <h1>ScienceMotions</h1>
-        <p>Biology, chemistry and physics lessons from the future.</p>
-      </div>
+        <div className="container">
+            <div className="col-lg-12">
+                <div className="header">
+                    <br/>
+                    <h1>Testimonials</h1>
+                    <br/>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-md-6 portfolio-item class">
+                    <p>
+                        <i>“It looks really cool! I use GCSE Bitesize but it doesn’t always explain things that well.”</i></p>
+                    <p> Blessing, GCSE Student, Brixton </p>
+                </div>
+                <div className="col-md-6 portfolio-item dna">
+                    <p>
+                        <i> “This looks like it would be very helpful. Right now I use Gojimo, but it just uses lots of questions”</i></p>
+                    <p>Joseph, A-Level Biology Student, Camberwell
+                    </p>
+                </div>
+            </div>
+        </div>
     );
   }
 }
