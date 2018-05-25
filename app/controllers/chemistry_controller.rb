a73bdacb95db37fb06997031d950255c964a04d1
Jessica Leach <jessica@info.nl>
2018-05-25 13:08:14 +0200

    no error method, no post for No route matches [POST] /chemistry/indicators

--- app/controllers/chemistry_controller.rb
+++ app/controllers/chemistry_controller.rb
@@ -1,4 +1,26 @@
 class ChemistryController < ApplicationController
   def indicators
   end
+
+  def show
+    @completed_lessons = @lesson.completed_lessons.all
+    @completed_lesson = @lesson.completed_lessons.build
+  end
+
+  def new
+    @indicator = Indicator.new
+  end
+
+  def update
+    respond_to do |format|
+      if @indicator.update(lesson_params)
+        format.html { redirect_to @indicator, notice: 'Lesson was successfully updated.' }
+        format.json { render :show, status: :ok, location: @indicator }
+      else
+        format.html { render :edit }
+        format.json { render json: @indicator.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
 end
