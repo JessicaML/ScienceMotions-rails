afd08559555afaf43d2e46937cd100bcc8e2e388
Jessica Leach <jessica@info.nl>
2018-05-25 11:09:33 +0200

    renders chemistry

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   resources :users do
     resources :completed_lessons
   end
-  get 'pages/info'
+  get 'pages/chemistry'
 
   root to: redirect('/lessons')  
   resources :lessons
