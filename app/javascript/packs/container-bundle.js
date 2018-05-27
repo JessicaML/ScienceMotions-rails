3cc259ea2ce0098f19cede10ef39893c37a5a87b
Jessica Leach <jessicamaryleach@gmail.com>
2018-05-27 16:51:20 +0200

    monika added ass dumb component

--- app/javascript/packs/container-bundle.js
+++ app/javascript/packs/container-bundle.js
@@ -2,7 +2,7 @@ import ReactOnRails from 'react-on-rails';
 
 import Container from '../bundles/Container/components/Container';
 import Lesson from '../bundles/Container/components/Lesson';
-import Team from '../bundles/Container/components/Team';
+import Team from '../bundles/Container/components/team/Team';
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
