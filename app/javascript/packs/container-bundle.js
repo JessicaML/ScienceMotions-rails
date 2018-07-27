f17e67340a1ba2a701594444a6b179d8f44b378b
Jessica Leach <jessica@info.nl>
2018-07-27 12:18:52 +0200

    added indicators lesson

--- app/javascript/packs/container-bundle.js
+++ app/javascript/packs/container-bundle.js
@@ -1,12 +1,14 @@
-import ReactOnRails from 'react-on-rails';
+import ReactOnRails from "react-on-rails";
 
-import Container from '../bundles/Container/components/Container';
-import Lesson from '../bundles/Container/components/Lesson';
-import Team from '../bundles/Container/components/team/Team';
+import Container from "../bundles/Container/components/Container";
+import Lesson from "../bundles/Container/components/Lesson";
+import Team from "../bundles/Container/components/team/Team";
+import Indicators from "../bundles/Container/components/Indicators";
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
   Container,
   Lesson,
-  Team,
+  Indicators,
+  Team
 });
