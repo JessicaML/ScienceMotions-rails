02c1d56126ad846b3b1650cb896cfe8f33569c64
Jessica Leach <jessica@info.nl>
2018-08-28 17:23:54 +0200

    add-text-indicators lesson and clean up

---
+++ app/javascript/bundles/Container/components/text/IndicatorsText.jsx
@@ -0,0 +1,12 @@
+import PropTypes from "prop-types";
+import React from "react";
+
+const IndicatorsText = ({}) => (
+  <section className="text">
+    <div className="table">
+      <p>kdshklwjeEF SKHDFLKSDFJ;O JSGGJ ;K L;S L:KDFGR3WLGK THGTGB FGHFG</p>
+    </div>
+  </section>
+);
+
+export default IndicatorsText;
