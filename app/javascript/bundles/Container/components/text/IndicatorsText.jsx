bced13467d3feb74281cd89e90d0aff96cc528c5
Jessica Leach <jessicamaryleach@gmail.com>
2018-09-16 16:01:54 +0200

    indicator text added

--- app/javascript/bundles/Container/components/text/IndicatorsText.jsx
+++ app/javascript/bundles/Container/components/text/IndicatorsText.jsx
@@ -4,7 +4,29 @@ import React from "react";
 const IndicatorsText = ({}) => (
   <section className="text">
     <div className="table">
-      <p>kdshklwjeEF SKHDFLKSDFJ;O JSGGJ ;K L;S L:KDFGR3WLGK THGTGB FGHFG</p>
+    <section id="text">
+        <h1>Indicators</h1>
+        <p>When an acid is dissolved in water we get an
+            <span id="text-1">acidic</span> solution. When a base dissolves in water it is an alkali and makes an
+            <span id="text-2">alkaline</span> solution. If a solution is neither acidic nor alkaline it is
+            <span id="text-3">neutral</span>. Pure water is neutral, and so is paraffin.</p>
+        <p>Indicators are substances that change colour when they are added to acidic or alkaline solutions. Litmus, phenolphthalein,
+            and methyl orange are all indicators that are commonly used in the laboratory.</p>
+
+        <p>
+            <span id="text-1">Litmus</span>
+        </p>
+        <p>Litmus indicator solution turns red in acidic solutions and blue in alkaline solutions - and it turns purple in neutral
+            solutions.
+        </p>
+        <p>
+            <span id="text-2">Litmus paper</span> is usually more reliable, and comes as red litmus paper and blue litmus paper. The table
+            shows the colour changes it can make.</p>
+
+        <p>The table above shows the colour changes it can make.</p>
+
+
+    </section>
     </div>
   </section>
 );
