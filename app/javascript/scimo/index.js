337a43c66785017b91098846727520d764b7a37c
Anne Elisabeth <anne.eckersberg@gmail.com>
2018-09-18 12:16:05 +0200

    import lesson_template in index.js file, change id into class in css

--- app/javascript/scimo/index.js
+++ app/javascript/scimo/index.js
@@ -3,6 +3,7 @@ import "./styles/style.css";
 import "./styles/Indicators.css";
 import "./styles/DNA.css";
 import "./styles/CellsTissuesOrgans.css";
+import "./styles/lesson_template.css"
 
 import "./scripts/DNA.js";
 import "./scripts/CellsTissuesOrgans.js";
