5fd763458a28d168374a8e62898e87d163ce14ea
Anne Elisabeth <anne.eckersberg@gmail.com>
2018-09-18 12:10:47 +0200

    refactor code, and add animationend event listener on tissue container animation

--- app/javascript/scimo/scripts/CellsTissuesOrgans.js
+++ app/javascript/scimo/scripts/CellsTissuesOrgans.js
@@ -4,32 +4,31 @@ function makeCell() {
     $newCell = $("<div class='cell-clone'></div>").clone().css({
         'left': posx + 'px',
         'top': posy + 'px',
-        
+
     });
 
     $newCell.appendTo('.tissue-container');
 }
 
-
-function multiplyCell(){
-    for(var i = 0; i < 120; i++){
-        setTimeout(function timer(){
+function multiplyCell() {
+    for (var i = 0; i < 120; i++) {
+        setTimeout(function timer() {
             makeCell();
-        }, i * 65); 
-    }           
+        }, i * 65);
+    }
 }
 
+setTimeout(multiplyCell, 11000);
 
-setTimeout( multiplyCell, 11000 );
+$(document).ready(function () {
+    $('.organ').hide();
+    $('.organ-system').hide();
+    var x = document.querySelector(".tissue-container");
 
-$(function(){
-    $('.organ').hide().delay(22000).queue(function(n) {
-        $(this).show(); n();
-    })
-});
-
-$(function(){
-$('.organ-system').hide().delay(26000).queue(function(n) {
-    $(this).show(); n();
-})
+    x.addEventListener("animationend", function () {
+        $('.organ').show();
+        $('.organ-system').hide().delay(4000).queue(function (n) {
+            $(this).show();
+        })
+    });
 });
