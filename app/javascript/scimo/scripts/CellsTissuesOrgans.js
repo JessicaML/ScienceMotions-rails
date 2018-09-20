f6476df7e2680d1c448602ac9e1f99df8221bade
Anne Elisabeth <anne.eckersberg@gmail.com>
2018-09-20 15:37:41 +0200

    rename variable x to tissue

--- app/javascript/scimo/scripts/CellsTissuesOrgans.js
+++ app/javascript/scimo/scripts/CellsTissuesOrgans.js
@@ -10,6 +10,7 @@ function makeCell() {
     $newCell.appendTo('.tissue-container');
 }
 
+
 function multiplyCell() {
     for (var i = 0; i < 120; i++) {
         setTimeout(function timer() {
@@ -18,14 +19,15 @@ function multiplyCell() {
     }
 }
 
+
 setTimeout(multiplyCell, 11000);
 
 $(document).ready(function () {
     $('.organ').hide();
     $('.organ-system').hide();
-    var x = document.querySelector(".tissue-container");
+    var tissue = document.querySelector(".tissue-container");
 
-    x.addEventListener("animationend", function () {
+    tissue.addEventListener("animationend", function () {
         $('.organ').show();
         $('.organ-system').hide().delay(4000).queue(function (n) {
             $(this).show();
