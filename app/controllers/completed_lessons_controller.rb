a0329377c4b4250fcc0fb89612fe09d8467b99ca
Anne Elisabeth <anne.eckersberg@gmail.com>
2018-09-25 11:18:58 +0200

    only find the current user completed lessons, remove completedlessons.save

--- app/controllers/completed_lessons_controller.rb
+++ app/controllers/completed_lessons_controller.rb
@@ -25,12 +25,12 @@ end
   # POST /completed_lessons
   # POST /completed_lessons.json
   def create
+
+
     @completed_lesson = CompletedLesson.new(completed_lesson_params)
     @completed_lesson.user = current_user
-    @completed_lesson.save
     
     respond_to do |format|
-
       if @completed_lesson.save
         format.html { redirect_to @completed_lesson, notice: 'Completed lesson was successfully created.' }
         format.json { render :show, status: :created, location: @completed_lesson }
@@ -68,7 +68,7 @@ end
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_completed_lesson
-      @completed_lesson = CompletedLesson.find(params[:id])
+      @completed_lesson = current_user.completed_lessons.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
