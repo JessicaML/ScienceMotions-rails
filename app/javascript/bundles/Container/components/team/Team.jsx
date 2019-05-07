146ec736896a9eeb34d560c22439d4eb27ad696e
Jessica Leach <jessica@info.nl>
2019-05-07 15:27:28 +0200

    monika img

--- app/javascript/bundles/Container/components/team/Team.jsx
+++ app/javascript/bundles/Container/components/team/Team.jsx
@@ -1,27 +1,12 @@
-import PropTypes from 'prop-types';
-import React from 'react';
+import React from "react";
 
-import Monika from './Monika';
+import Monika from "./Monika";
 
-export default class Team extends React.Component {
-  static propTypes = {
-    name: PropTypes.string.isRequired, // this is passed from the Rails view
-  };
+const Team = ({}) => (
+  <div>
+    <h1>Team</h1>
+    <Monika />
+  </div>
+);
 
-  /**
-   * @param props - Comes from your rails view.
-   */
-  constructor(props) {
-    super(props);
-
-    // How to set initial state in ES6 className syntax
-    // https://reactjs.org/docs/state-and-lifecycle.html#adding-local-state-to-a-className
-  }
-
-  render() {
-    return (
-      <Monika />
-
-    );
-  }
-}
+export default Team;
