e567fddeebd0be729f5cf4dd9f5615504da27789
Jessica Leach <jessica@info.nl>
2019-05-09 17:34:46 +0200

    install npm package for mailchimp

--- app/javascript/bundles/Container/components/MailingList.jsx
+++ app/javascript/bundles/Container/components/MailingList.jsx
@@ -1,61 +1,73 @@
 import React from "react";
+import Mailchimp from "react-mailchimp-form";
 
 const MailingList = () => (
-  <div id="footer" className="container">
-    <div class="row">
-      <div class="col-md-12 portfolio-item">
-        <div id="mc_embed_signup">
-          <form
-            action="//jhn.us16.list-manage.com/subscribe/post-json?u=70d8e0ea2d8800a5f4e06792c&amp;id=b5095bc718&c=?"
-            method="post"
-            id="mc-embedded-subscribe-form"
-            name="mc-embedded-subscribe-form"
-            class="validate"
-            target="_blank"
-            novalidate
-          >
-            <div id="mc_embed_signup_scroll">
-              <div>
-                <span>
-                  {" "}
-                  <i>
-                    <p>
-                      Science is about how shapes move in space, so we think
-                      animation is the best way to explain it... and we want to
-                      explain it in a way that's as fun to look at as MTV.
-                    </p>
-                  </i>
-                </span>
-              </div>
-              <label className="subscription" for="mce-EMAIL">
-                Subscribe to our mailing list!
-              </label>
-              <input
-                type="email"
-                value=""
-                name="EMAIL"
-                class="email form-control"
-                id="mce-EMAIL"
-                placeholder="email address"
-                required
-              />
+  <Mailchimp
+    action="//jhn.us16.list-manage.com/subscribe/post-json?u=70d8e0ea2d8800a5f4e06792c&amp;id=b5095bc718&c=?"
+    fields={[
+      {
+        name: "EMAIL",
+        placeholder: "Email",
+        type: "email",
+        required: true
+      }
+    ]}
+  />
+  // <div id="footer" className="container">
+  //   <div className="row">
+  //     <div className="col-md-12 portfolio-item">
+  //       <div id="mc_embed_signup">
+  //         <form
+  //           action="//jhn.us16.list-manage.com/subscribe/post-json?u=70d8e0ea2d8800a5f4e06792c&amp;id=b5095bc718&c=?"
+  //           method="post"
+  //           id="mc-embedded-subscribe-form"
+  //           name="mc-embedded-subscribe-form"
+  //           className="validate"
+  //           target="_blank"
+  //           noValidate
+  //         >
+  //           <div id="mc_embed_signup_scroll">
+  //             <div>
+  //               <span>
+  //                 {" "}
+  //                 <i>
+  //                   <p>
+  //                     Science is about how shapes move in space, so we think
+  //                     animation is the best way to explain it... and we want to
+  //                     explain it in a way that's as fun to look at as MTV.
+  //                   </p>
+  //                 </i>
+  //               </span>
+  //             </div>
+  //             <label className="subscription" htmlFor="mce-EMAIL">
+  //               Subscribe to our mailing list!
+  //             </label>
+  //             <input
+  //               type="email"
+  //               value=""
+  //               name="EMAIL"
+  //               className="email form-control"
+  //               id="mce-EMAIL"
+  //               placeholder="email address"
+  //               required
+  //             />
 
-              <div class="clear">
-                <input
-                  type="submit"
-                  value="Subscribe"
-                  name="subscribe"
-                  id="mc-embedded-subscribe"
-                  class="btn"
-                />
-              </div>
-            </div>
-            <div id="notification_container" />
-          </form>
-        </div>
-      </div>
-    </div>
-    <script src="js/jquery.js" />
-  </div>
+  //             <div className="clear">
+  //               <input
+  //                 type="submit"
+  //                 value="Subscribe"
+  //                 name="subscribe"
+  //                 id="mc-embedded-subscribe"
+  //                 className="btn"
+  //               />
+  //             </div>
+  //           </div>
+  //           <div id="notification_container" />
+  //         </form>
+  //       </div>
+  //     </div>
+  //   </div>
+  //   <script src="js/jquery.js" />
+  // </div>
 );
 export default MailingList;
