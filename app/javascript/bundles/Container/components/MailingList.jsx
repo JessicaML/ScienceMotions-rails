c2909acd88f4c0a5f952a3aab16cf77d5ddf561c
Jessica Leach <jessica@info.nl>
2019-05-10 10:15:50 +0200

    removing new window on mailchimp

--- app/javascript/bundles/Container/components/MailingList.jsx
+++ app/javascript/bundles/Container/components/MailingList.jsx
@@ -1,4 +1,10 @@
 import React from "react";
+import $ from "jquery";
+
+{
+  /* <script type='text/javascript' src='//s3.amazonaws.com/downloads.mailchimp.com/js/mc-validate.js'></script>
+<script type='text/javascript'>(function($) {window.fnames = new Array(); window.ftypes = new Array();fnames[0]='EMAIL';ftypes[0]='email';fnames[1]='FNAME';ftypes[1]='text';fnames[2]='LNAME';ftypes[2]='text';}(jQuery));var $mcj = jQuery.noConflict(true);</script> */
+}
 
 class MailingList extends React.Component {
   constructor(props) {
@@ -10,13 +16,64 @@ class MailingList extends React.Component {
     };
   }
 
+  onSubmit() {
+    console.log("-----onSubmit!");
+    preventDefault();
+
+    $.ajax({
+      type: $form.attr("method"),
+      url: $form.attr("action"),
+      data: $form.serialize(),
+      cache: false,
+      dataType: "json",
+      contentType: "application/json; charset=utf-8",
+      error: function(err) {
+        $(".notification_container").html(
+          '<span className="alert">Could not connect to server. Please try again later.</span>'
+        );
+      },
+      success: function(data) {
+        if (data.result != "success") {
+          var message = data.msg.substring();
+          $(".notification_container").html(
+            '<span className="alert">' + message + "</span>"
+          );
+        } else {
+          var message = data.msg;
+          $(".notification_container").html(
+            '<span className="success">' + message + "</span>"
+          );
+        }
+      }
+    });
+  }
+
   render() {
     return (
       <form
-        action="https://github.us16.list-manage.com/subscribe/post"
+        action="https://github.us16.list-manage.com/subscribe/post-json"
         method="POST"
         noValidate
+        id="mc-embedded-subscribe-form"
+        name="mc-embedded-subscribe-form"
+        className="validate"
+        target="_blank"
       >
+        <div id="mc_embed_signup_scroll">
+          <div>
+            <span style={{ color: "#fff" }}>
+              <i>
+                <p>
+                  Science is about how shapes move in space, so we think
+                  animation is the best way to explain it... and we want to
+                  explain it in a way that's as fun to look at as MTV.
+                </p>
+              </i>
+            </span>
+          </div>
+        </div>
+        <label for="mce-EMAIL">Subscribe to our mailing list!</label>
+
         <input type="hidden" name="u" value="70d8e0ea2d8800a5f4e06792c" />
         <input type="hidden" name="id" value="b5095bc718" />
         <label htmlFor="MERGE0">
@@ -33,53 +90,19 @@ class MailingList extends React.Component {
             autoCorrect="off"
           />
         </label>
-        <label htmlFor="MERGE1">
-          First name
-          <input
-            type="text"
-            name="FNAME"
-            id="MERGE1"
-            value={this.state.fNameValue}
-            onChange={e => {
-              this.setState({ fNameValue: e.target.value });
-            }}
-          />
-        </label>
-        <label htmlFor="MERGE2">
-          Last name
-          <input
-            type="text"
-            name="LNAME"
-            id="MERGE2"
-            value={this.state.lNameValue}
-            onChange={e => {
-              this.setState({ lNameValue: e.target.value });
-            }}
-          />
-        </label>
         <input
           type="submit"
+          onClick={this.onSubmit}
           value="Subscribe"
           name="subscribe"
           id="mc-embedded-subscribe"
           className="button"
         />
-
         <div
           style={{ position: "absolute", left: "-5000px" }}
           aria-hidden="true"
           aria-label="Please leave the following three fields empty"
         >
-          <label htmlFor="b_name">Name: </label>
-          <input
-            type="text"
-            name="b_name"
-            tabIndex="-1"
-            value=""
-            placeholder="Freddie"
-            id="b_name"
-          />
-
           <label htmlFor="b_email">Email: </label>
           <input
             type="email"
@@ -89,15 +112,8 @@ class MailingList extends React.Component {
             placeholder="youremail@gmail.com"
             id="b_email"
           />
-
-          <label htmlFor="b_comment">Comment: </label>
-          <textarea
-            name="b_comment"
-            tabIndex="-1"
-            placeholder="Please comment"
-            id="b_comment"
-          />
         </div>
+        <div className="notification_container" />
       </form>
     );
   }
