2d6604798f5c18c5e65a1c3c11c9e7df4f185de7
Jessica Leach <jessica@info.nl>
2019-05-21 09:17:02 +0200

    mouse over sound wave works

--- app/javascript/bundles/Container/components/lessons/Sound.jsx
+++ app/javascript/bundles/Container/components/lessons/Sound.jsx
@@ -3,28 +3,51 @@ import SoundPng from "images/sound.png";
 import SoundGif from "images/sound.gif";
 import SoundText from "./text/SoundText";
 
-const Sound = () => (
-  <Fragment>
-    <section id="visual">
-      <div id="nav-one" className="nav">
-        <audio id="beep-one">
-          <source
-            src="https://cdn.rawgit.com/JessicaML/ScienceMotion-Static/aa82537e/sound_waves/wouldnt_say.mp3"
-            controls
-          />
-          <source
-            src="https://cdn.rawgit.com/JessicaML/ScienceMotion-Static/aa82537e/sound_waves/wouldnt_say.ogg"
-            controls
-          />
-          Your browser isn't invited for super fun time.
-        </audio>
-        <a href="#" id="sound">
-          <img src={SoundPng} onMouseOver={SoundGif} onMouseOut={SoundPng} />
-        </a>
-      </div>
-    </section>
-    <SoundText />
-  </Fragment>
-);
+class Sound extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { imgSrc: SoundPng };
+    this.soundGif = this.soundGif.bind(this);
+    this.soundPng = this.soundPng.bind(this);
+  }
 
+  soundGif() {
+    this.setState({ imgSrc: SoundGif });
+  }
+
+  soundPng() {
+    this.setState({ imgSrc: SoundPng });
+  }
+
+  render() {
+    const { imgSrc } = this.state;
+    return (
+      <Fragment>
+        <section id="visual">
+          <div id="nav-one" className="nav">
+            <audio id="beep-one">
+              <source
+                src="https://cdn.rawgit.com/JessicaML/ScienceMotion-Static/aa82537e/sound_waves/wouldnt_say.mp3"
+                controls
+              />
+              <source
+                src="https://cdn.rawgit.com/JessicaML/ScienceMotion-Static/aa82537e/sound_waves/wouldnt_say.ogg"
+                controls
+              />
+              Your browser isn't invited for super fun time.
+            </audio>
+            <a href="#" id="sound">
+              <img
+                src={imgSrc}
+                onMouseOver={this.soundGif}
+                onMouseOut={this.soundPng}
+              />
+            </a>
+          </div>
+        </section>
+        <SoundText />
+      </Fragment>
+    );
+  }
+}
 export default Sound;
