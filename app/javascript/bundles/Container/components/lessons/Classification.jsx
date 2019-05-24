eca84d6fe83eae2da3c7dca178f5a7045a8b7fef
Jessica Leach <jessica@info.nl>
2019-05-24 13:16:00 +0200

    added classification lesson

---
+++ app/javascript/bundles/Container/components/lessons/Classification.jsx
@@ -0,0 +1,54 @@
+import React, { Fragment } from "react";
+import ClassificationText from "./text/ClassificationText";
+import classnames from "classnames";
+
+const phrases = [
+  "Movement - they can move and change their position.",
+  "Reproduction – they can make more of the same kind of organism as themselves.",
+  "Sensitivity – they can detect or sense stimuli and respond to them.",
+  "Growth - they can permanently increase their size or dry mass by increasing the number or size of their cells.",
+  "Respiration – they can create chemical reactions that break down nutrient molecules in living cells to release energy.",
+  "Excretion – they can excrete toxic materials, waste products of metabolism, and excess substances (note that excretion is not the same as egestion).",
+  "Nutrition - they can take in and absorb nutrients such as organic substances and mineral ions. These nutrients contain the raw materials or energy needed for growth and tissue repair."
+];
+
+const styles = ["zero", "one", "two", "three", "four", "five", "six"];
+
+class Classification extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      phrase: phrases[0],
+      value: 0
+    };
+  }
+
+  changeInput = e => {
+    const newPhrase = phrases[e.target.value];
+    this.setState({ phrase: newPhrase, value: e.target.value });
+  };
+
+  render() {
+    const { phrase, value } = this.state;
+    const classNames = classnames("phrase", styles[value]);
+    return (
+      <Fragment>
+        <section id="visual" className="classification">
+          <div id="header">
+            <h1>Characteristics of living organisms </h1>
+            <input
+              type="range"
+              defaultValue="0"
+              min="0"
+              max="6"
+              onChange={this.changeInput}
+            />
+          </div>
+          <div className={classNames}>{phrase}</div>
+        </section>
+        <ClassificationText />
+      </Fragment>
+    );
+  }
+}
+export default Classification;
---
+++ app/javascript/bundles/Container/components/lessons/text/ClassificationText.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+const ClassificationText = ({}) => (
+  <section className="text">
+    <div>
+      <h1>Classification</h1>
+      <p>
+        The first letter of each of these characteristics makes up the acronym
+        <span id="text-1"> MRS GREN</span>. This is a good way of remembering
+        them.
+      </p>
+      <p>
+        Living organisms can
+        <span id="text-2"> reproduce</span>
+      </p>
+      <p>
+        Living organisms can also control their
+        <span id="text-3"> internal conditions</span>, such as their temperature
+        or water content.
+      </p>
+    </div>
+  </section>
+);
+
+export default ClassificationText;
