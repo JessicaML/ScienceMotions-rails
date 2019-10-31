69b6434b6ec9e30314aa50f8df8611341d0a91f7
tob <tobia.donati@stepstone.com>
2019-10-31 15:53:33 +0000

    feat/add skrollr to dna and update css using flex

--- app/javascript/bundles/Container/components/lessons/DNA.jsx
+++ app/javascript/bundles/Container/components/lessons/DNA.jsx
@@ -1,4 +1,5 @@
 import React, { Fragment } from "react";
+import { ParallaxProvider, Parallax } from 'react-skrollr'
 import Adenine from "images/adenine.png";
 import Cytosine from "images/cytosine.png";
 import Guanine from "images/guanine.png";
@@ -7,20 +8,30 @@ import DNAText from "./text/DNAText";
 
 const DNA = () => (
   <Fragment>
-    <section id="visual">
-      <div className="table">
-        <div className="row">
-          <div className="col-sm-1" />
-          <div className="col-sm-3">
-            <img src={Cytosine} className="dna-img c" />
-            <img src={Thymine} className="dna-img t" />
-            <img src={Adenine} className="dna-img a" />
-            <img src={Guanine} className="dna-img g" />
+    <ParallaxProvider>
+      <Parallax
+        data={{
+          'data-top': 'opacity: 0;',
+          'data-bottom-center': 'opacity: 1;',
+          'data-bottom': 'opacity: 0;'
+        }}
+      >
+        <section id="visual">
+          <div className="table">
+            <div className="row">
+              <div className="col-sm-3 dna-img-container">
+                <div className="dna-img-span" ><img src={Cytosine} className="dna-img c" /></div>
+                <div className="dna-img-span"><img src={Thymine} className="dna-img t" /></div>
+                <div className="dna-img-span"><img src={Adenine} className="dna-img a" /></div>
+                <div className="dna-img-span"><img src={Guanine} className="dna-img g" /></div>
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
-    </section>
+        </section>
+      </Parallax>
+    </ParallaxProvider>
     <DNAText />
+
   </Fragment>
 );
 
--- app/javascript/scimo/scripts/DNA.js
+++ app/javascript/scimo/scripts/DNA.js
@@ -1,13 +1,13 @@
 $(document).on("mousemove", function(event) {
-  $("img.t").css("width", event.pageX);
+  $("img.t").css("width", $("#top").width()- event.pageX);
   $("img.t").css("height", event.pageY);
 
-  $("img.c").css("width", $("#top").width() - event.pageX);
-  $("img.c").css("height", event.pageY);
+  $("img.c").css("width", event.pageX);
+  $("img.c").css("height", $("#top").height() - event.pageY);
 
   $("img.a").css("width", event.pageX);
   $("img.a").css("height", $("#top").height() - event.pageY);
 
-  $("img.g").css("width", event.pageX);
-  $("img.g").css("height", $("#top").height() - event.pageY);
+  $("img.g").css("width", $("#top").width()- event.pageX);
+  $("img.g").css("height", event.pageY);
 });
