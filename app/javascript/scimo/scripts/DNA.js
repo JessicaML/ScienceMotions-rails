2fd7f9fb37900b764b6d496dcff43862f0c73c0b
Jessica Leach <jessica.leach@futurelearn.com>
2019-11-02 09:56:51 +0100

    fixed positioning in mobile DNA lesson

--- app/javascript/scimo/scripts/DNA.js
+++ app/javascript/scimo/scripts/DNA.js
@@ -2,12 +2,12 @@ $(document).on("mousemove", function(event) {
   $("img.t").css("width", event.pageX + 20);
   $("img.t").css("height", event.pageY);
 
-  $("img.c").css("width", $("#visual").width() - event.pageX + 400);
+  $("img.c").css("width", $("#visual").width() - event.pageX);
   $("img.c").css("height", event.pageY);
 
   $("img.a").css("width", event.pageX);
-  $("img.a").css("height", $("#visual").height() - event.pageY + 20);
+  $("img.a").css("height", $("#visual").height() - event.pageY);
 
-  $("img.g").css("width", $("#visual").width() - event.pageX + 400);
-  $("img.g").css("height", $("#visual").height() - event.pageY + 200);
+  $("img.g").css("width", $("#visual").width() - event.pageX);
+  $("img.g").css("height", $("#visual").height() - event.pageY);
 });
