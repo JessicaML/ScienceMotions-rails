8f4b05f23527087f4c9525152fb5cc22a73ab79f
Jessica Leach <jessica.leach@futurelearn.com>
2019-12-21 17:16:40 +0100

    lesson test pass

--- spec/models/lesson_spec.rb
+++ spec/models/lesson_spec.rb
@@ -1,7 +1,8 @@
 require "rails_helper"
 
 RSpec.describe Lesson, type: :model do
-  it "has a name" do
-    skip
+  it "has a name" do 
+    lesson = Lesson.create!(name: "Lesson Name 1") 
+    expect(lesson.name).to eq("Lesson Name 1") 
   end
 end
