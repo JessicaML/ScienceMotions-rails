7bde48c3297c71cea9249d8779c98d1c621ac705
Jessica Leach <jessica.leach@futurelearn.com>
2019-12-22 15:02:42 +0100

    webpack prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -32,7 +32,7 @@ config.webpacker.check_yarn_integrity = false
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
