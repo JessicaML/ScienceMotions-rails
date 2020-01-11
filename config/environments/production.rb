f40065cbe0377873b92706e2efb419e75e9e4655
Roman Klevtsov <contact@rubyvkube.ru>
2020-01-12 04:35:42 +0800

    fix: remove assets, packs folders
    fix: puma config preload & estabilish connection

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -33,7 +33,7 @@ config.webpacker.check_yarn_integrity = false
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-
+  config.assets.digest = true
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
