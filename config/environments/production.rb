86f2ac9148c4a495811467b061b0f9ebcf31ff67
Roman Klevtsov <contact@rubyvkube.ru>
2020-01-12 01:36:35 +0800

    fix: back RAILS_SERVE_STATIC_FILES conf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,8 +24,8 @@ config.webpacker.check_yarn_integrity = false
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  # config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
-    config.public_file_server.enabled = true
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+    # config.public_file_server.enabled = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
