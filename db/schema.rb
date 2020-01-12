29df59043a36efe0e18bc07435d5e0fd254c5e38
Jessica Leach <jessica.leach@futurelearn.com>
2020-01-12 15:14:57 +0000

    ActiveRecord::Schema.define(version: 2018_12_08_163425) do

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181208163425) do
+ActiveRecord::Schema.define(version: 2018_12_08_163425) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
