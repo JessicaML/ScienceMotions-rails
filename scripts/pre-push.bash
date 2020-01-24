448a9630c2a542f86301505b7ebde1e386a875d7
Jessica Leach <jessica.leach@futurelearn.com>
2020-01-24 16:09:43 +0000

    can i commit?

--- scripts/pre-push.bash
+++ scripts/pre-push.bash
@@ -3,6 +3,6 @@ echo "Running pre-push hook"
 
 # $? stores exit value of the last command
 if [ $? -ne 0 ]; then
- echo "Brakeman and Tests must pass before pushing!"
+ echo " Tests must pass before pushing!"
  exit 1
 fi
