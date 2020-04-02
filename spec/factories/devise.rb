850046892cbdece1596f4433b22c7d257efcf87b
stephanie-warmenhoven <steph_war@yahoo.ca>
2020-04-01 23:11:05 -0700

    removed commented code because completed lessons has own factory

--- spec/factories/devise.rb
+++ spec/factories/devise.rb
@@ -12,12 +12,6 @@ FactoryBot.define do
       slug {"slug"}
     end
 
-    # factory :completed_lesson do
-    #   lesson_id {1}
-    #   user_id {1}
-    #   completed {true}
-    # end
-
     # Not used in this tutorial, but left to show an example of different user types
     # factory :admin do
     #   id {2}
@@ -26,3 +20,4 @@ FactoryBot.define do
     #   admin {true}
     # end
   end
+  
