d003bd59906e10de2be2667a8edb68a07312667f
Jessica Leach <jessica.leach@futurelearn.com>
2020-04-10 19:10:24 -0400

    added dangerously set innerHTML

--- app/javascript/bundles/Container/components/lessons/Atoms.js
+++ app/javascript/bundles/Container/components/lessons/Atoms.js
@@ -1,11 +1,11 @@
 import React, { Fragment } from "react";
-import AtomsText from "./text/AtomsText";
+import Text from "./text/Text";
 import eye1 from "../../../../../assets/images/eye1.png";
 import eye2 from "../../../../../assets/images/eye2.png";
 
 const Atoms = (props) => (
   <Fragment>
-  {console.log('props', props)}
+  {/* {console.log('props', props)} */}
     <section id="visual" className="lesson-container">
       <div id="atoms">
         <div className="eye1">
@@ -99,7 +99,7 @@ const Atoms = (props) => (
         </svg>
       </div>
     </section>
-    <AtomsText />
+    <Text props={props} />
   </Fragment>
 );
 
--- app/javascript/bundles/Container/components/lessons/text/Text.js
+++ app/javascript/bundles/Container/components/lessons/text/Text.js
@@ -1,22 +1,13 @@
 import React from "react";
 
-const AtomsText = (props) => (
+const Text = (props) => (
   <section id="text">
     {console.log('props', props)}
 
-    <div>
-      <h1>Atomic structure</h1>
-      <p>{props}</p>
-      <p>
-        All substances are made from tiny particles called
-        <span id="text-1"> atoms</span>. An atom has a small central nucleus
-        made up of smaller sub-atomic particles called
-        <span id="text-2"> protons and neutrons</span>. The nucleus is
-        surrounded by even smaller sub-atomic particles called
-        <span id="text-3"> electrons</span>.
-      </p>
-    </div>
+    {/* <div>{props.props.description}</div>
+    <div dangerouslySetInnerHTML={createMarkup()} /> */}
+    <div dangerouslySetInnerHTML={{__html: props.props.description}} />
   </section>
 );
 
-export default AtomsText;
+export default Text;
