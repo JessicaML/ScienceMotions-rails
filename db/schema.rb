a1a5fc4c3adac326d18f7ec8357f96581ffea87c
Jessica Leach <jessica.leach@futurelearn.com>
2020-04-10 19:20:44 -0400

    migration to add subject to lessons

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_12_08_163425) do
+ActiveRecord::Schema.define(version: 2020_04_10_231447) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2018_12_08_163425) do
     t.string "slug"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "subject"
   end
 
   create_table "models", force: :cascade do |t|
