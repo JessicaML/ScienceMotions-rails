182c05e85f0d7bbeca78ee0c38fc5563cd158ff9
Jessica Leach <jessica.leach@futurelearn.com>
2020-04-25 10:11:35 -0400

    remove comments

--- spec/support/controller_macros.rb
+++ spec/support/controller_macros.rb
@@ -1,22 +1,21 @@
 FactoryBot.define do
     module ControllerMacros
         
-        def login_user
-        # Before each test, create and login the user
+    def login_user
+       # Before each test, create and login the user
         before(:each) do
             @request.env["devise.mapping"] = Devise.mappings[:user]
             user = FactoryBot.create(:user)
             # user.confirm! # Or set a confirmed_at inside the factory. Only necessary if you are using the "confirmable" module
             sign_in user
         end
-        end
+    end
 
-    # Not used in this tutorial, but left to show an example of different user types
-    # def login_admin
-    #   before(:each) do
-    #     @request.env["devise.mapping"] = Devise.mappings[:admin]
-    #     sign_in FactoryBot.create(:admin) # Using factory bot as an example
-    #   end
-    # end
+    def login_admin
+      before(:each) do
+        @request.env["devise.mapping"] = Devise.mappings[:admin]
+        sign_in FactoryBot.create(:admin) # Using factory bot as an example
+      end
+    end
   end
-end  
+end
