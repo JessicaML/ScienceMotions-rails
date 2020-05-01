52ee92f2402e26452b8645fbfd23bdf7fccbbf59
Aline Ribeiro <alinear.mail@gmail.com>
2020-04-30 18:47:36 -0700

    fixed undefined with error

--- app/javascript/bundles/Container/components/lessons/StatesOfMatter.js
+++ app/javascript/bundles/Container/components/lessons/StatesOfMatter.js
@@ -1,22 +1,32 @@
 import React, { Fragment } from "react";
 import Text from "./text/Text";
 
-
-const StatesOfMatter = (props) => (
-  <Fragment>
-  {console.log('props', props)}
-    <section id="visual" className="lesson-container">
-    <div class="buttons">
-      <button id="solid" onClick="changeState(500)">SOLID</button>
-      <button id="liquid" onClick="changeState(200)">LIQUID</button>
-      <button id="gas" onClick="changeState(50)">GAS</button>
-    </div>
-    <canvas id="myCanvas" width="1000" height="1000" style="border:0px solid #000000;">
-
-    </canvas>
-    </section>
-    <Text props={props} />
-  </Fragment>
-);
+const StatesOfMatter = props => {
+  return (
+    <Fragment>
+      <h1>Test</h1>
+      <section id="visual" className="lesson-container">
+        <div className="buttons">
+          <button id="solid" onClick="changeState(500)">
+            SOLID
+          </button>
+          <button id="liquid" onClick="changeState(200)">
+            LIQUID
+          </button>
+          <button id="gas" onClick="changeState(50)">
+            GAS
+          </button>
+        </div>
+        <canvas
+          ref="myCanvas"
+          width="1000"
+          height="1000"
+          style={{border: 0, solid: '#000000'}}
+        ></canvas>
+      </section>
+      <Text props={props.description} />
+    </Fragment>
+  );
+};
 
 export default StatesOfMatter;
