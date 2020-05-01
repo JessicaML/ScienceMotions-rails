f3f603ffab8849b1ef80a2088fab7cecbd1cebfd
Aline Ribeiro <alinear.mail@gmail.com>
2020-04-30 19:36:31 -0700

    added useRef hook

--- app/javascript/bundles/Container/components/lessons/StatesOfMatter.js
+++ app/javascript/bundles/Container/components/lessons/StatesOfMatter.js
@@ -1,32 +1,38 @@
-import React, { Fragment } from "react";
+import React, { useRef } from "react";
 import Text from "./text/Text";
 
-const StatesOfMatter = props => {
+const StatesOfMatter = (props) => {
+   const canvas = useRef(null);
+
+  const changeState =  function () {
+
+   };
+
   return (
-    <Fragment>
-      <h1>Test</h1>
+    <div>
       <section id="visual" className="lesson-container">
         <div className="buttons">
-          <button id="solid" onClick="changeState(500)">
+          <button id="solid" onClick={changeState(500)}>
             SOLID
           </button>
-          <button id="liquid" onClick="changeState(200)">
+          <button id="liquid" onClick={changeState(200)}>
             LIQUID
           </button>
-          <button id="gas" onClick="changeState(50)">
+          <button id="gas" onClick={changeState(50)}>
             GAS
           </button>
         </div>
         <canvas
-          ref="myCanvas"
+          ref={canvas}
           width="1000"
           height="1000"
-          style={{border: 0, solid: '#000000'}}
+          style={{ border: 0, solid: "#000000" }}
         ></canvas>
       </section>
       <Text props={props.description} />
-    </Fragment>
+    </div>
   );
 };
 
-export default StatesOfMatter;
+// export default StatesOfMatter;
+export default props => <StatesOfMatter {...props} />
