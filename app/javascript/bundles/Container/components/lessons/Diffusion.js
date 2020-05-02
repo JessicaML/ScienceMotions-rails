4feedaafb9dbe901fcf85b6a02ea730090b23882
stephanie-warmenhoven <steph_war@yahoo.ca>
2020-05-01 17:50:03 -0700

    Rendered lesson but upside down

--- app/javascript/bundles/Container/components/lessons/Diffusion.js
+++ app/javascript/bundles/Container/components/lessons/Diffusion.js
@@ -1,21 +1,13 @@
 import React, { Fragment } from "react";
+import DiffusionScript from "./DiffusionScript";
 import Text from "./text/Text";
-import { ParallaxProvider, Parallax } from "react-skrollr";
 
 const Diffusion = (props) => (
-  <Fragment>
-      data={{
-        "data-0":
-          "background-image: linear-gradient(90deg, hsl(172, 100%, 50%), hsl(318, 68%, 68%)",
-        "data-450":
-          "background-image: linear-gradient(450deg, hsl(172, 100%, 50%), hsl(318, 68%, 68%)"
-      }}
-    >
-    <section id="visual" className="diffusion">
-      
-    </section>
-    <Text props={this.props.description} />
+   <Fragment>
+     <section id="visual" className="diffusion" className="lesson-container">
+     <DiffusionScript props={props} />
+     </section>
+    <Text props={props.description} />
   </Fragment>
 );
-
-export default Diffusion;
+export default props => <Diffusion {...props} />;
---
+++ app/javascript/bundles/Container/components/lessons/DiffusionScript.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useRef } from "react";
+
+const DiffusionScript = props => {
+var width = 960,
+height = 500;
+
+var nodes = [];
+
+var svg = d3.select("body").append("svg")
+.attr("width", width)
+.attr("height", height);
+
+var force = d3.layout.force()
+.charge(-20)
+.size([width, height])
+.nodes(nodes)
+.on("tick", tick)
+.start();
+
+function tick() {
+svg.selectAll("circle")
+    .attr("cx", function (d) { return d.x; })
+    .attr("cy", function (d) { return d.y; });
+}
+
+var interval = setInterval(function () {
+var d = {
+    x: width / 2 + 2 * Math.random() - 1,
+    y: height / 2 + 2 * Math.random() - 1
+};
+
+svg.append("circle")
+    .data([d])
+    .attr("r", 1e-6)
+    .transition()
+    .ease(Math.sqrt)
+    .attr("r", 4.5);
+
+if (nodes.push(d) > 300) clearInterval(interval);
+force.start();
+}, 30);
+
+return (
+    <script></script>
+  );
+};
+
+export default props => <DiffusionScript {...props} />;
