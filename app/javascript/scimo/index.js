4a5c68a76347afaba5c6aa24b27602b137718321
stephanie-warmenhoven <steph_war@yahoo.ca>
2020-05-05 23:49:26 -0700

    It_works.patch animation without buttons

--- app/javascript/scimo/index.js
+++ app/javascript/scimo/index.js
@@ -11,7 +11,7 @@ import "./styles/CellsTissuesOrgans.css";
 import "./styles/lesson_template.css";
 import "./styles/SolarSystem.css";
 import "./styles/AlkaliMetals.scss";
-import "./styles/AlkaliMetals2.css";
+import "./styles/AlkaliMetals2.scss";
 import "./styles/_base.css";
 
 
