89b26efe2746a3bd1140235f36d7b1703374f498
Jessica Leach <jessica.leach@futurelearn.com>
2020-05-08 11:36:16 -0400

    fix typo

--- app/javascript/scimo/index.js
+++ app/javascript/scimo/index.js
@@ -11,7 +11,7 @@ import "./styles/CellsTissuesOrgans.css";
 import "./styles/lesson_template.css";
 import "./styles/SolarSystem.css";
 import "./styles/AlkaliMetals.scss";
-import "./scripts/GroupsPeriods.css";
+import "./styles/GroupsPeriods.css";
 
 import "./scripts/DNA.js";
 import "./scripts/Sound.js";
