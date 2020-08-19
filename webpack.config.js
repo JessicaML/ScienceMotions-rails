dcbbf008776dffa0e193a27ddb8f8fb43dccd8db
Jessica Leach <jessica.leach@futurelearn.com>
2020-08-19 20:11:01 +0100

    try adding webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        exclude: /node_modules/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
