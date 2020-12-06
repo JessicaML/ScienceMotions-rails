f02e749f7ab7619193e0d2b2d99ee6f137556bd3
Jessica Leach <jessica.leach@futurelearn.com>
2020-12-06 15:23:42 +0000

    tidy routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,26 @@
 Rails.application.routes.draw do
-
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
-
-  get 'container', to: 'container#index'
-  get 'team', to: 'team#index'
-  resources :completed_lessons do
-    put "togglelesson"
-  end
-  
-  resources :lessons, param: :slug
+ 
+  root to: redirect('/container')
 
   devise_for :users do
     get "/users/sign_out" => "devise/sessions#destroy", :as => :destroy_user_session
   end
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+
   resources :users do
     resources :completed_lessons
   end
 
-  get "completed_lessons/:id/togglelesson" => "completed_lessons#togglelesson", :as => "togglelesson", :via => [:get]
+  get 'container', to: 'container#index'
+  get 'team', to: 'team#index'
+  resources :lessons, param: :slug
 
-  root to: redirect('/container')
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get "/static/:index" => "static#show"
-  get "/static/:navigation" => "static#show"
+  resources :completed_lessons do
+    put "togglelesson"
+  end
+  
+  get "completed_lessons/:id/togglelesson" => "completed_lessons#togglelesson", :as => "togglelesson", :via => [:get]
 end
 
