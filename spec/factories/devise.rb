6dfff172121476f6840fffa3f45f5f9cba4053be
Jessica Leach <jessica.leach@futurelearn.com>
2020-12-16 17:03:02 +0000

    feature spec complete

--- spec/factories/devise.rb
+++ spec/factories/devise.rb
@@ -1,11 +1,4 @@
 FactoryBot.define do
-
-    # factory :lesson do
-    #     name {"test"}
-    #     description {"qwerty"}
-    #     slug {"slug"}
-    # end
-  
     factory :admin do
       id {2}
       email {"test@admin.com"}
--- spec/features/marking_lessons_as_complete_spec.rb
+++ spec/features/marking_lessons_as_complete_spec.rb
@@ -15,11 +15,14 @@ RSpec.describe 'Marking lessons as complete', type: :feature do
     then_i_go_to_the_lesson_page
   end
 
-  # scenario 'marking a lesson as complete' do
-  # end
-
-  # scenario 'when_i_go_the_login_page' do
-  # end
+  scenario 'marking a lesson as complete' do
+    and_i_click_the_lesson
+    then_i_go_to_the_lesson_page
+    and_i_click_the_mark_complete_button
+    then_i_see_that_the_lesson_has_been_marked_complete
+    and_i_visit_the_completed_lessons_page
+    and_the_completed_lesson_is_there
+  end
 
   def when_i_go_to_the_home_page
     visit container_path
@@ -45,5 +48,22 @@ RSpec.describe 'Marking lessons as complete', type: :feature do
 
   def then_i_go_to_the_lesson_page
     expect(page).to have_selector("input", :class =>"mark-complete")
+  end
+     
+  def and_i_click_the_mark_complete_button
+    click_button 'mark complete'
+  end
+
+  def then_i_see_that_the_lesson_has_been_marked_complete
+    expect(page).to have_content('Lesson Complete')
+  end
+
+  def and_i_visit_the_completed_lessons_page
+    visit completed_lessons_path
+  end
+
+  def and_the_completed_lesson_is_there
+    expect(page).to have_content(@lesson.name)
+    expect(page).to have_selector("a", :text =>"mark incomplete")
   end
 end
